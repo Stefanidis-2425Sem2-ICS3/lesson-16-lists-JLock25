8b783575dc4302e86a83b704dd11d986cace5e8f
JLock25 <jwgaming462@gmail.com>
2025-03-24 12:37:43 -0400

    done

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+# Name: J. Lockey
+# Title: random number
+# Date: March 24, 2025
+# Description: This program takes 100 random numbers from 1-100 and finds the average between them all
+import random
+random_numbers = [random.randint(1, 100) for i in range(100)]
+average = sum(random_numbers) / len(random_numbers)
+print(f"Random numbers: {random_numbers}")
+print(f"Average of the random numbers are: {average}")
